01ecf2d646d775c657c22e8c707da77b41cd5bc9
김정현 <wjdgus0117@ccmedia.co.kr>
2025-06-19 17:14:52 +0900

    [Feat]: login.html 수정

---
+++ src/main/java/com/kjung/springoauth/core/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.kjung.springoauth.core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+//    private final OAuth2UserService oAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                                .loginPage("/login")
+//                        .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+}
